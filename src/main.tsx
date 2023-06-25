2395e0fefbed07fa9a7356e14fc9ad529cbf3034
Andrii Shtohryn <andriishtohryn@gmail.com>
2023-06-25 16:55:17 +0300

    add hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import { BrowserRouter } from "react-router-dom";
-import { createHashRouter, RouterProvider } from "react-router-dom";
-const router = createHashRouter([
-  {
-    path: "/follow-cards",
-    element: <App />,
-  },
-]);
+import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
